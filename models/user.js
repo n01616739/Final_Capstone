be0380286e4fb5033a6eed845385d51a1ac458ca
Siddharth <n01616739@humber.ca>
2025-02-04 13:07:21 -0500

    Ui changes

--- models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-  username: String,
-  password: String,
-  role: { type: String, default: 'user' } // Default role is 'user'
-});
-
-module.exports = mongoose.model('user', userSchema);
